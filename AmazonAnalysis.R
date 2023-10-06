6a2f19741e5a888327e77b3875146800c54ad8a2
Tyler <tyler.c.smith04@gmail.com>
2023-10-06 14:42:38 -0600

    edit recipe

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -13,7 +13,7 @@ test <- vroom("./test.csv")
 my_recipe <- recipe(ACTION ~ ., data = train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
   step_other(all_nominal_predictors(), threshold = .01) %>% 
-  step_dummy(all_nominal_predictors()) %>% # dummy variable encoding
+  step_dummy(all_nominal_predictors()) # dummy variable encoding
 
 
 # NOTE: some of these step functions are not appropriate to use together 13
