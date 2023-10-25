6af0cbc1c10d68251479299f0493ee28b6bbc19d
Tyler <tyler.c.smith04@gmail.com>
2023-10-25 12:01:12 -0600

    svm

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -73,11 +73,11 @@ predict_and_format <- function(workflow, new_data, filename){
 
 # Penalized Regression
 
-# target_encoding_recipe <- recipe(ACTION ~ ., data = train) %>%
-#   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
-#   step_other(all_nominal_predictors(), threshold = .001) %>% 
-#   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% # dummy variable encoding
-#   step_normalize(all_nominal_predictors())
+target_encoding_recipe <- recipe(ACTION ~ ., data = train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% # dummy variable encoding
+  step_normalize(all_nominal_predictors())
 # 
 # 
 # pen_log_mod <- logistic_reg(mixture = tune() , penalty = tune() ) %>%
@@ -229,12 +229,12 @@ predict_and_format <- function(workflow, new_data, filename){
 # predict_and_format(final_knn_wf, test, "./knn_predictions.csv")
 
 # Principal Component Dimension Reduction ---------------------------------
-pcdr_recipe <- recipe(ACTION ~ ., train) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
-  step_other(all_nominal_predictors(), threshold = .001) %>%  # combines categorical values that occur <1% into an "other" value
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%  # target encoding (must be 2-factor)
-  step_normalize(all_nominal_predictors()) %>% 
-  step_pca(all_predictors(), threshold = .8) #Threshold is between 0 and 1
+# pcdr_recipe <- recipe(ACTION ~ ., train) %>%
+#   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
+#   step_other(all_nominal_predictors(), threshold = .001) %>%  # combines categorical values that occur <1% into an "other" value
+#   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%  # target encoding (must be 2-factor)
+#   step_normalize(all_nominal_predictors()) %>% 
+#   step_pca(all_predictors(), threshold = .8) #Threshold is between 0 and 1
 
 # nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
 #   set_mode('classification') %>%
@@ -303,40 +303,81 @@ pcdr_recipe <- recipe(ACTION ~ ., train) %>%
 # predict_and_format(final_knn_wf, test, "./knn_pcdr_predictions.csv")
 
 # Random Forests w PCDR ---------------------------------------------------
-rand_forest_mod <- rand_forest(mtry = tune(),
-                               min_n=tune(),
-                               trees=1000) %>% # or 1000
-  set_engine("ranger") %>%
-  set_mode("classification")
+# rand_forest_mod <- rand_forest(mtry = tune(),
+#                                min_n=tune(),
+#                                trees=1000) %>% # or 1000
+#   set_engine("ranger") %>%
+#   set_mode("classification")
+# 
+# rand_forest_workflow <- workflow() %>%
+#   add_recipe(pcdr_recipe) %>%
+#   add_model(rand_forest_mod)
+# 
+# rand_forest_tuning_grid <- grid_regular(mtry(range = c(1, (ncol(train)-1))),
+#                                         min_n(),
+#                                         levels = 5) ## L^2 total tuning possibilities
+# 
+# ## Split data for CV
+# forest_folds <- vfold_cv(train, v = 5, repeats = 1)
+# 
+# ## Run the CV
+# CV_results <- rand_forest_workflow %>%
+#   tune_grid(resamples = forest_folds,
+#             grid = rand_forest_tuning_grid,
+#             metrics = metric_set(roc_auc)) # f_meas, sens, recall, spec, precision, accuracy
+# 
+# ## Find Best Tuning Parameters
+# forest_bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# ## Finalize the Workflow & fit it
+# final_forest_wf <- rand_forest_workflow %>%
+#   finalize_workflow(forest_bestTune) %>%
+#   fit(data = train)
+# 
+# predict_and_format(final_forest_wf, test, "./random_forest_pcdr_predictions.csv")
 
-rand_forest_workflow <- workflow() %>%
-  add_recipe(pcdr_recipe) %>%
-  add_model(rand_forest_mod)
+# Support Vector Machines -------------------------------------------------
+svmPoly <- svm_poly(degree = tune(), cost = tune()) %>% 
+  set_mode('classification') %>% 
+  set_engine('kernlab')
 
-rand_forest_tuning_grid <- grid_regular(mtry(range = c(1, (ncol(train)-1))),
-                                        min_n(),
-                                        levels = 5) ## L^2 total tuning possibilities
+svmRadial <- svm_rbf(rbf_sigma = tune(), cost = tune()) %>% 
+  set_mode('classification') %>% 
+  set_engine('kernlab')
 
-## Split data for CV
-forest_folds <- vfold_cv(train, v = 5, repeats = 1)
+svmLinear <- svm_linear(cost = tune()) %>% 
+  set_mode('classification') %>% 
+  set_engine('kernlab')
+
+svm_wf <- workflow() %>%
+  add_recipe(target_encoding_recipe) %>%
+  add_model(svmRadial)
+
+# cross validation
+svm_tuning_grid <- grid_regular(cost(),
+                                rbf_sigma(),
+                                levels = 5)
+
+svm_folds <- vfold_cv(train, v = 5, repeats = 1)
 
 ## Run the CV
-CV_results <- rand_forest_workflow %>%
-  tune_grid(resamples = forest_folds,
-            grid = rand_forest_tuning_grid,
-            metrics = metric_set(roc_auc)) # f_meas, sens, recall, spec, precision, accuracy
+CV_results <- knn_wf %>%
+  tune_grid(resamples = knn_folds,
+            grid = knn_tuning_grid,
+            metrics = metric_set(roc_auc))
 
-## Find Best Tuning Parameters
-forest_bestTune <- CV_results %>%
+svm_bestTune <- CV_results %>%
   select_best("roc_auc")
 
-## Finalize the Workflow & fit it
-final_forest_wf <- rand_forest_workflow %>%
-  finalize_workflow(forest_bestTune) %>%
+# finalize workflow
+final_svm_wf <- knn_wf %>%
+  finalize_workflow(knn_bestTune) %>%
   fit(data = train)
 
-predict_and_format(final_forest_wf, test, "./random_forest_pcdr_predictions.csv")
+predict_and_format(final_svm_wf, test, "./svm_predictions.csv")
 
 stopCluster(cl)
+
 
 
