4a7bda6537d89b64571ce31b35dc8f7a772ebc5f
Tyler <tyler.c.smith04@gmail.com>
2023-10-28 14:04:37 -0600

    update smote

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -24,11 +24,17 @@ train <- vroom("./train.csv") %>%
 test <- vroom("./test.csv") %>% 
   select(-1)
 
+# balance_recipe <- recipe(ACTION ~ ., data = train) %>% 
+#   step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
+#   step_other(all_nominal_predictors(), threshold = 0.05) %>% 
+#   step_dummy(all_nominal_predictors()) %>% 
+#   step_smote(all_outcomes(), neighbors = 10)
+
 balance_recipe <- recipe(ACTION ~ ., data = train) %>% 
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
   step_other(all_nominal_predictors(), threshold = 0.05) %>% 
-  step_dummy(all_nominal_predictors()) %>% 
-  step_smote(all_outcomes(), neighbors = 10)
+  step_dummy(all_nominal_predictors(), -all_outcomes()) %>% 
+  step_smote(ACTION, neighbors = 10)
+
 
 # my_recipe <- recipe(ACTION ~ ., data = train) %>%
 #   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
